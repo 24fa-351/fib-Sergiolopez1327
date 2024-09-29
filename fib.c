e7de3cc8d16d7afa464fda62540a77a16c489d3d
Sergiolopez1327 <Sergiolopez132799@csu.fullerton.edu>
2024-09-28 19:11:20 -0700

    Added Overflow Check

--- fib.c
+++ fib.c
@@ -1,6 +1,14 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+int fibOverflow(int a, int b) {
+    if (a > (INT_MAX - b)) {
+        return 1;
+    }
+    return 0;
+}
 
 int fib_iterative(int fibIndex) {
     if (fibIndex <= 2) {
@@ -12,6 +20,10 @@ int fib_iterative(int fibIndex) {
 
     for (int iter = 3; iter <= fibIndex; iter++) {
         int next;
+        if (fibOverflow(previous, current)) {
+            fprintf(stderr, "Overflow detected at index %d\n", iter + 2);
+            return -1;
+        }
         next = previous + current;
         previous = current;
         current = next;
@@ -24,21 +36,28 @@ int fib_recursive(int fibIndex) {
     if (fibIndex <= 2) {
         return fibIndex - 1;
     }
+    int firstfibvalue = fib_recursive(fibIndex - 1);
+    int secondfibvalue = fib_recursive(fibIndex - 2);
+    if (fibOverflow(firstfibvalue, secondfibvalue)) {
+        fprintf(stderr, "Overflow detected at index %d\n", fibIndex);
+        return -1;
+    }
 
-    return fib_recursive(fibIndex - 1) + fib_recursive(fibIndex - 2);
+    return firstfibvalue + secondfibvalue;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     if (argc != 4) {
-        fprintf(stderr, "Usage: %s <input number> <fib method> <filename>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <input number> <fib method> <filename>\n",
+                argv[0]);
         return 1;
     }
 
     int input_number = atoi(argv[1]);
     char fib_method = argv[2][0];
-    char *filename = argv[3];
+    char* filename = argv[3];
 
-    FILE *file = fopen(filename, "r");
+    FILE* file = fopen(filename, "r");
     if (file == NULL) {
         perror("Error opening the file");
         return 1;
@@ -56,7 +75,9 @@ int main(int argc, char *argv[]) {
     } else if (fib_method == 'r') {
         fib_result = fib_recursive(N);
     } else {
-        fprintf(stderr, "Invalid method. Use 'i' for iterative or 'r' for recursive.\n");
+        fprintf(
+            stderr,
+            "Invalid method. Use 'i' for iterative or 'r' for recursive.\n");
         return 1;
     }
 
